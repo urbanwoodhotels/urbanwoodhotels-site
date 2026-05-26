f358ac7762e680f7282557fc2c41d769dd05f803
urbanwoodhotels <urbanwoodhotels@gmail.com>
2026-05-26 01:45:05 +0000

    fix mobile answer flicker

--- client/src/lib/imageConfig.ts
+++ client/src/lib/imageConfig.ts
@@ -22,32 +22,32 @@ export const imageConfig: {
 } = {
   cover: {
     desktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180154/urbanwood-quiz/enxo8cz5d44zwwnn6me3.jpg',
-    mobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180154/urbanwood-quiz/enxo8cz5d44zwwnn6me3.jpg',
+    mobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759174/Online-Test-Result-cover-deisgn-phone_rgwn65.jpg',
   },
 
   chapters: {
     1: {
       coverDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180171/urbanwood-quiz/gcdgkauygbgerqgpktyv.jpg',
-      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180171/urbanwood-quiz/gcdgkauygbgerqgpktyv.jpg',
-      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180171/urbanwood-quiz/gcdgkauygbgerqgpktyv.jpg',
+      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759175/Online-Test-Result-cover-deisgn-phone-07_n12fth.jpg',
+      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759159/Online-Test-Result-cover-deisgn-02_cckv0o.jpg',
       questionMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180171/urbanwood-quiz/gcdgkauygbgerqgpktyv.jpg',
     },
     2: {
       coverDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181295/urbanwood-quiz/cef3e1pdixgsmn57uyyc.jpg',
-      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181295/urbanwood-quiz/cef3e1pdixgsmn57uyyc.jpg',
-      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181295/urbanwood-quiz/cef3e1pdixgsmn57uyyc.jpg',
+      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759173/Online-Test-Result-cover-deisgn-phone-08_bueluv.jpg',
+      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759160/Online-Test-Result-cover-deisgn-04_bg0itr.jpg',
       questionMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181295/urbanwood-quiz/cef3e1pdixgsmn57uyyc.jpg',
     },
     3: {
       coverDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181301/urbanwood-quiz/iatistgbtcfek96cbbpu.jpg',
-      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181301/urbanwood-quiz/iatistgbtcfek96cbbpu.jpg',
-      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181301/urbanwood-quiz/iatistgbtcfek96cbbpu.jpg',
+      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759173/Online-Test-Result-cover-deisgn-phone-08_bueluv.jpg',
+      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759160/Online-Test-Result-cover-deisgn-03_xfrryd.jpg',
       questionMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779181301/urbanwood-quiz/iatistgbtcfek96cbbpu.jpg',
     },
     4: {
-      coverDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180187/urbanwood-quiz/bzqqbevvtbwbk7cvzxn0.jpg',
-      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180187/urbanwood-quiz/bzqqbevvtbwbk7cvzxn0.jpg',
-      questionDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180187/urbanwood-quiz/bzqqbevvtbwbk7cvzxn0.jpg',
+      coverDesktop: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759144/Ch.4_v2_acykvy.jpg',
+      coverMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779759174/Online-Test-Result-cover-deisgn-phone-10_awgn2u.jpg',
+      questionDesktop: 'https://rehttps://res.cloudinary.com/defqvpbk4/image/upload/v1779759158/Online-Test-Result-cover-deisgn-05_xarzwz.jpg',
       questionMobile: 'https://res.cloudinary.com/defqvpbk4/image/upload/v1779180187/urbanwood-quiz/bzqqbevvtbwbk7cvzxn0.jpg',
     },
   },
